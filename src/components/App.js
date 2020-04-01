6e19af7a89c39b7307b1d40a609ed3a15f14599b
leopoldkwok <leopoldkwok@gmail.com>
2020-04-01 17:37:05 +0100

    removing items from state

--- src/components/App.js
+++ src/components/App.js
@@ -56,6 +56,15 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  deleteFish = (key) => {
+    // 1. take a copy of state
+    const fishes = { ...this.state.fishes };
+    // 2. update the state
+    fishes[key] = null;
+    // 3. update state
+    this.setState({ fishes });
+  }
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes })
   };
@@ -68,6 +77,15 @@ class App extends React.Component {
     // 3. call setState to update our state object
     this.setState({ order });
   }
+
+  removeFromOrder = key => {
+    // 1. take a copy of state
+    const order = { ...this.state.order };
+    // 2. remove that item from order
+    delete order[key];
+    // 3. call setState to update our state object
+    this.setState({ order });
+  };
 
   render() {
     return (
@@ -88,10 +106,12 @@ class App extends React.Component {
           <Order
             fishes={this.state.fishes} 
             order={this.state.order}
+            removeFromOrder={this.removeFromOrder}
           />
           <Inventory 
             addFish={this.addFish}
             updateFish={this.updateFish}
+            deleteFish={this.deleteFish}
             loadSampleFishes={this.loadSampleFishes}
             fishes={this.state.fishes}
           />
